772a73c5fe2a14f13fe18c2d4fa1d44d1bfc491a
PenelopeFarias745 <156312541+PenelopeFarias745@users.noreply.github.com>
2024-04-17 19:40:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,13 +14,13 @@ Webcam.attach('#camera');
 function takeSnapshot()
 {
   Webcam.snap(function(data_uri){
-    document.getElementById("result").innerHMTL = '<img id="captured_image" src="'+data_uri+'">';
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
   });
 }
 
 console.log("ml5 version:", ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/DtDdqetnZ/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/jaUdxuWEU/model.json', modelLoaded);
 
 function modelLoaded()
 {
@@ -30,15 +30,15 @@ function modelLoaded()
 function speak()
 {
   var synth = window.speechSynthesis;
-  speakData1 = "A primeira previsão é" + predicition1;
-  speakData2 = "A primeira previsão é" + predicition2;
+  speakData1 = "A primeira previsão é" + prediction1;
+  speakData2 = "A segunda previsão é" + prediction2;
   var utterThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
   synth.speak(utterThis);
 }
 
 function check()
 {
-  img = document.getElementById("captured_image");
+  img = document.getElementById('captured_image');
   classifier.classify(img, gotResult);
 }
 
@@ -53,7 +53,7 @@ function gotResult(error, results)
     console.log(results);
     document.getElementById("resultGestureName").innerHTML = results[0].label;
     document.getElementById("resultGestureName2").innerHTML = results[1].label;
-    predicition1 = results[0].label;
+    prediction1 = results[0].label;
     prediction2 = results[1].label;
     speak();
 
@@ -65,9 +65,9 @@ function gotResult(error, results)
     {
       document.getElementById("updateEmoji").innerHTML = "&#9996;";
     }
-    if(results[0].label == "Ok")
+    if(results[0].label == "Toca aqui")
     {
-      document.getElementById("updateEmoji").innerHTML = "&#129306;";
+      document.getElementById("updateEmoji").innerHTML = "&#9995;";
     }
     if(results[1].label == "Joinha")
     {
@@ -77,9 +77,9 @@ function gotResult(error, results)
     {
       document.getElementById("updateEmoji2").innerHTML = "&#9996;";
     }
-    if(results[1].label == "Ok")
+    if(results[1].label == "Toca aqui")
     {
-      document.getElementById("updateEmoji2").innerHTML = "&#129306;";
+      document.getElementById("updateEmoji2").innerHTML = "&#9995;";
     }
   }
 }
